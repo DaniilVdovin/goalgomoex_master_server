27fdef73e19c2cf74edf326b228f342b073ba585
Dannil Vdovin <stels1040533@gmail.com>
2023-12-02 14:03:48 +0300

    init calc profit dto

---
+++ src/main/java/ru/goalgomoex/goalgomoex/entitys/dto/task/calcProfit/dtoSignal.java
@@ -0,0 +1,7 @@
+package ru.goalgomoex.goalgomoex.entitys.dto.task.calcProfit;
+
+import ru.goalgomoex.goalgomoex.entitys.dto.IDTOEntity;
+
+public class dtoSignal implements IDTOEntity {
+
+}
---
+++ src/main/java/ru/goalgomoex/goalgomoex/entitys/dto/task/calcProfit/dtoTaskOutCalcProfit.java
@@ -0,0 +1,9 @@
+package ru.goalgomoex.goalgomoex.entitys.dto.task.calcProfit;
+
+import ru.goalgomoex.goalgomoex.entitys.dto.task.dtoTaskOut;
+
+public class dtoTaskOutCalcProfit extends dtoTaskOut {
+    private dtoSignal singals_example;
+    public dtoTaskOutCalcProfit() {
+    }
+}
