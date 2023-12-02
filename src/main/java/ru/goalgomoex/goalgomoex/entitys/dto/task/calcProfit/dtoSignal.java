39063d6305d64fed060755b3e357f90c5fb496ce
Dannil Vdovin <stels1040533@gmail.com>
2023-12-02 14:30:15 +0300

    calc profit dto done

--- src/main/java/ru/goalgomoex/goalgomoex/entitys/dto/task/calcProfit/dtoSignal.java
+++ src/main/java/ru/goalgomoex/goalgomoex/entitys/dto/task/calcProfit/dtoSignal.java
@@ -1,7 +1,36 @@
 package ru.goalgomoex.goalgomoex.entitys.dto.task.calcProfit;
 
 import ru.goalgomoex.goalgomoex.entitys.dto.IDTOEntity;
+import ru.goalgomoex.goalgomoex.entitys.dto.task.dtoValueDV;
 
 public class dtoSignal implements IDTOEntity {
+    private dtoValueDV<float[]> markup_signals;
+    private dtoValueDV<float[]> neural_signals;
+    private dtoValueDV<int[]> neural_trends;
 
+    public dtoSignal() {
+    }
+    public dtoValueDV<float[]> getMarkup_signals() {
+        return markup_signals;
+    }
+
+    public void setMarkup_signals(dtoValueDV<float[]> markup_signals) {
+        this.markup_signals = markup_signals;
+    }
+
+    public dtoValueDV<float[]> getNeural_signals() {
+        return neural_signals;
+    }
+
+    public void setNeural_signals(dtoValueDV<float[]> neural_signals) {
+        this.neural_signals = neural_signals;
+    }
+
+    public dtoValueDV<int[]> getNeural_trends() {
+        return neural_trends;
+    }
+
+    public void setNeural_trends(dtoValueDV<int[]> neural_trends) {
+        this.neural_trends = neural_trends;
+    }
 }
---
+++ src/main/java/ru/goalgomoex/goalgomoex/entitys/dto/task/dtoData.java
@@ -0,0 +1,29 @@
+package ru.goalgomoex.goalgomoex.entitys.dto.task;
+
+import ru.goalgomoex.goalgomoex.entitys.dto.IDTOEntity;
+
+public class dtoData<D,V> implements IDTOEntity {
+    private D X;
+    private V Y;
+    public dtoData() {
+    }
+    public dtoData(D x, V y) {
+        X = x;
+        Y = y;
+    }
+    public D getX() {
+        return X;
+    }
+
+    public void setX(D x) {
+        X = x;
+    }
+
+    public V getY() {
+        return Y;
+    }
+
+    public void setY(V y) {
+        Y = y;
+    }
+}
