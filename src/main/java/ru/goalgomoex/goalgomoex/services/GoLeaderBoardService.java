e8d45d1afab96e35d6181f8f9ffebd30e405d75d
Dannil Vdovin <stels1040533@gmail.com>
2023-12-06 22:23:23 +0300

    fix lb

---
+++ src/main/java/ru/goalgomoex/goalgomoex/services/GoLeaderBoardService.java
@@ -0,0 +1,37 @@
+package ru.goalgomoex.goalgomoex.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ru.goalgomoex.goalgomoex.entitys.goLeaderBoardItem;
+import ru.goalgomoex.goalgomoex.entitys.goScriptConfig;
+import ru.goalgomoex.goalgomoex.repository.GoConfRepository;
+import ru.goalgomoex.goalgomoex.repository.GoLeaderBoardRepository;
+
+import java.util.List;
+
+@Service
+public class GoLeaderBoardService {
+    @Autowired private GoLeaderBoardRepository goLeaderBoardRepository;
+    public List<goLeaderBoardItem> list(){
+        return goLeaderBoardRepository.findAll();
+    }
+    public List<goLeaderBoardItem> listByTimeframe(String timeframe){
+        return goLeaderBoardRepository.findByTimeframe(timeframe);
+    }
+    public List<goLeaderBoardItem> listForService(String timeframe){
+        return goLeaderBoardRepository.findByTimeframe(timeframe);
+    }
+    public goLeaderBoardItem createOrUpdate(goLeaderBoardItem object){
+        if(object == null) return null;
+        object.upVersion();
+        return goLeaderBoardRepository.saveAndFlush(object);
+    }
+    public List<goLeaderBoardItem> createOrUpdate(List<goLeaderBoardItem> object){
+        if(object == null) return null;
+        object.forEach(goLeaderBoardItem::upVersion);
+        return goLeaderBoardRepository.saveAllAndFlush(object);
+    }
+    public goLeaderBoardItem Get(long id) {
+        return goLeaderBoardRepository.findById(id).orElse(new goLeaderBoardItem());
+    }
+}
